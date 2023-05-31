3aae0981db90360620ec429cd18d741f1eba0de5
Ben Harding <bharding@ualberta.ca>
2023-05-31 11:27:10 -0700

    call modules

--- src/main.f90
+++ src/main.f90
@@ -1,10 +1,25 @@
 program main
 
    use readpar_mod
+   use mtmod
+   use impute_mod, only: impute
+   use output_mod, only: write_files
 
    implicit none
 
    ! read the parfile
    call readpar
+
+   ! initilize random generator
+   call sgrnd(rseed)
+
+   ! impute the factors
+   call impute
+
+   ! write output files
+   call write_files
+
+   ! close all output files
+   close (lout)
 
 end program main
