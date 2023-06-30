69ab51065eeb7762651a1a937631fcb32267b560
Ben Harding <bharding@ualberta.ca>
2023-06-30 11:25:15 -0700

    comments

--- src/main.f90
+++ src/main.f90
@@ -1,11 +1,26 @@
 program main
 
+   ! Network model of regionalization imputation. Latent
+   ! Gaussian factors are imputed using Sequential Gaussian
+   ! Rejection Impuation. Imputed factors honour the variograms
+   ! specified by the pool and reproduce the hard data values
+   ! when passed through the optimized network generated
+   ! with NMROPT.
+   !
+   ! Author: Ben Harding
+   ! Date: June 2023
+   ! Location: Centre for Computational Geostatistics,
+   ! University of Alberta, Edmonton, Canada
+   ! Contact: bharding@ualberta.ca
+
    use readpar_mod
    use mtmod
    use impute_mod, only: impute
    use output_mod, only: write_files
 
    implicit none
+
+   real, parameter :: VERSION = 1.000
 
    ! read the parfile
    call readpar
@@ -21,5 +36,10 @@ program main
 
    ! close all output files
    close (lout)
+   close (ldbg)
+
+   ! finished
+   write (*, *) " "
+   write (*, "(A,f5.3,A)") "NMRIMP version ", VERSION, " finished"
 
 end program main
