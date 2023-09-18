0aefa8db9fe570cfd76d4f45f128fd8cf31249e4
Ben Harding <bharding@ualberta.ca>
2023-09-18 13:03:03 -0700

    refactor global variables

--- src/geostat.f90
+++ src/geostat.f90
@@ -17,12 +17,40 @@ module geostat
 
    ! simulation variables
    real(8), allocatable :: imputed(:, :, :) ! (ndata, nfact, nreals)
-   real(8), allocatable :: zinit(:, :)
+   real(8), allocatable :: zinit(:, :) ! (ndata, nfact)
+   integer, allocatable :: isim(:, :) ! (ndata, nfact)
+   real(8), allocatable :: sim(:, :) ! (ndata, nfact)
+   real(8), allocatable :: anisxyz(:, :)
+   integer, allocatable :: randpath(:)
    integer :: nsearch
    integer :: nreals
+   integer :: nfact
+   integer :: nst
    integer :: rseed
    integer :: iter1, iter2
    real(8) :: tol1, tol2
+
+   ! normal equations
+   real(8), allocatable :: rhs(:), lhs(:, :), kwts(:)
+   real(8), allocatable :: cmeans(:), cstdevs(:)
+   integer, allocatable :: nuse(:, :), useidx(:, :)
+
+   ! reference distribution
+   integer, parameter :: nsamp = 1000000
+   integer :: nlook
+   real(8) :: zref(nsamp)
+   real(8), allocatable :: nsref(:), yref(:, :)
+   real(8) :: usnsref(nsamp)
+   real(8) :: minref, maxref
+   real(8) :: gmin, gmax
+
+   ! dynamic tolerances
+   real(8), allocatable :: vsort(:), wsort(:), vord(:)
+   real(8), allocatable :: vcdf(:)
+
+   ! kdtree required variables
+   type(kdtrees), allocatable :: trees(:) ! array of pointers
+   type(kdtree2_result), allocatable :: results(:)
 
    ! data variables
    real(8), allocatable :: var(:)
